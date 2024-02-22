e340f3b3b870bc9fb6ab6a63e091621093e76fdd
Crazy Alert <sid.visceos@gmail.com>
2024-02-22 11:49:25 +1000

    Добавлены комменты в FileLock.php

--- FileLock.php
+++ FileLock.php
@@ -1,31 +1,57 @@
 <?php
 
+/**
+ * Класс, представляющий собой механизм блокировки файла.
+ */
 class FileLock {
+    /**
+     * @var string Путь к файлу блокировки.
+     */
     private $lockFile;
 
+    /**
+     * Конструктор класса.
+     *
+     * @param string $lockFile Путь к файлу блокировки.
+     */
     public function __construct($lockFile) {
         $this->lockFile = $lockFile;
     }
 
-    public function Lock():bool {
-    try {
-        if (file_exists($this->lockFile)) {
-            unlink($this->lockFile);
+    /**
+     * Получает блокировку файла.
+     *
+     * @return bool Успешность установки блокировки.
+     */
+    public function Lock(): bool {
+        try {
+            if (file_exists($this->lockFile)) {
+                // Если файл блокировки существует, он удаляется.
+                unlink($this->lockFile);
+            }
+            // Записывает в файл идентификатор процесса, который установил блокировку.
+            return file_put_contents($this->lockFile, getmypid());
+
+        } catch (Throwable $e) {
+            // В случае ошибки возвращает false.
+            return false;
         }
-        return file_put_contents($this->lockFile, getmypid());
-   
-    } catch (Throwable $e) {
-        return false;
-    }
     }
 
-    public function UnLock():bool {
-      try{
-        if (file_exists($this->lockFile)) {
-            return unlink($this->lockFile);
+    /**
+     * Снимает блокировку файла.
+     *
+     * @return bool Успешность снятия блокировки.
+     */
+    public function UnLock(): bool {
+        try {
+            if (file_exists($this->lockFile)) {
+                // Если файл блокировки существует, он удаляется.
+                return unlink($this->lockFile);
+            }
+        } catch (Throwable $e) {
+            // В случае ошибки возвращает false.
+            return false;
         }
-      }catch(Throwable $e){
-        return false;
-      }
     }
 }
